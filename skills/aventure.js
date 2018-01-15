73e7fa54ca154aba1c48486df46fc42c17937a71
Guillaume Bouillon <guillaume.bouillon@o-agence.fr>
2018-01-15 10:46:40 +0100

    [ADD] LDVELH

---
+++ skills/aventure.js
@@ -0,0 +1,83 @@
+"use strict";
+module.exports = function (controller) {
+
+    var chapters = require('../ldvelh/mysteres-de-sylandra.json');
+
+
+    controller.hears(['^aventure'], 'direct_message,direct_mention', function (bot, message) {
+
+        // console.log("bot", bot);
+        // console.log("message", message);
+
+        bot.startPrivateConversation(message, function (err, convo) {
+
+            for (var i = 0; i < chapters.length; i++) {
+                var chapter = chapters[i];
+                var chapter_buttons = [];
+                var chapter_options = [];
+
+                for (var j = 0; j < chapter.options.length; j++) {
+                    var curr_option = chapter.options[j];
+                    chapter_buttons.push({
+                        name: curr_option.title,
+                        text: curr_option.title,
+                        value: "page_" + curr_option.action,
+                        type: "button"
+                    });
+                    chapter_options.push({
+                        pattern: "page_" + curr_option.action,
+                        callback: function (response, convo) {
+                            bot.replyInteractive(response, {text: response.original_message.text + "\n> *" + response.actions[0].name + "*"});
+                            convo.gotoThread(response.actions[0].value);
+                        }
+                    });
+                }// endfor(chapter.options)
+
+                convo.addQuestion({
+                    text: chapter.text,
+                    attachments: [{
+                        fallback: "Choisir une action",
+                        callback_id: "page_" + chapter.id,
+                        actions: chapter_buttons
+                    }]
+                }, chapter_options, {}, "page_" + chapter.id + "");
+            } // endfor(chapters)
+
+            convo.addQuestion({
+                text: "Je suis pret à te faire vivre une aventure !",
+                attachments: [{
+                    text: "Veux-tu continuer ?",
+                    fallback: "Choisir une action",
+                    callback_id: 'default',
+                    actions: [{
+                        name: "oui",
+                        text: "Oui",
+                        value: 'yes',
+                        type: "button"
+                    }, {
+                        name: "non",
+                        text: "Non",
+                        value: false,
+                        type: "button"
+                    }]
+                }]
+            }, [{
+                pattern: "yes",
+                callback: function (response, convo) {
+                    bot.replyInteractive(response, {text: "C'est parti !"});
+                    convo.gotoThread('page_1');
+                }
+            }, {
+                default: true,
+                callback: function (response, convo) {
+                    bot.replyInteractive(response, {text: "tu n'es pas prêt ?\n OK! À bientôt."});
+                    convo.next();
+                }
+            }], {}, 'default');
+
+            convo.activate();
+        });
+
+    });
+
+};
--- skills/help.js
+++ skills/help.js
@@ -1,46 +1,33 @@
-module.exports = function(controller) {
+"use strict";
+module.exports = function (controller) {
 
-  controller.hears('^help$','direct_message,direct_mention',function(bot,message) {
-    
-    var reply_with_attachments = {
-      'username': 'Maurice' ,
-      'text': 'Comment puis-je vous aider ?',
-      'attachments': [
-        {
-          'fallback': 'Je raconte des blagues',
-          'title': 'Je raconte des blagues',
-          'text': '- Quand on me le demande avec `fact` \n'+
-                  '- Je peux énumérer celles que je connais: `facts` \n'+
-                  '- Tu peux m\'en apprendre avec `addfact _blague_` \n' +
-                  '- Si tu as un Flashouilleur... `remfact _numéro_` fonctionne aussi.',
-          'color': '#7CD197'
-        }
-        //,
-        // {
-        //   'title': 'Do you want to interact with my buttons?',
-        //   'callback_id': '123',
-        //   'attachment_type': 'default',
-        //   'actions': [
-        //     {
-        //       'name':'yes',
-        //       'text': 'Yes',
-        //       'value': 'yes',
-        //       'type': 'button',
-        //     },
-        //     {
-        //       'name':'no',
-        //       'text': 'No',
-        //       'value': 'no',
-        //       'type': 'button',
-        //     }
-        //   ]
-        // }
-      ]
-    }
+    controller.hears('^help$', 'direct_message,direct_mention', function (bot, message) {
 
-    bot.reply(message, reply_with_attachments);
-    
-  });
+        var reply_with_attachments = {
+            'username': 'Maurice',
+            'text': 'Comment puis-je vous aider ?',
+            'attachments': [
+                {
+                    'fallback': 'Je raconte des blagues',
+                    'title': 'Je raconte des blagues',
+                    'text': '- Quand on me le demande avec `fact` \n' +
+                    '- Je peux énumérer celles que je connais: `facts` \n' +
+                    '- Tu peux m\'en apprendre avec `addfact _blague_` \n' +
+                    '- Si tu as un Flashouilleur... `remfact _numéro_` fonctionne aussi.',
+                    'color': '#7CD197',
+                    'thumb_url': ''
+                },
+                {
+                    'fallback': 'Tu veux vivre une super aventure ?',
+                    'title': 'Tu veux vivre une super aventure ?',
+                    'text': 'Demande le moi avec `aventure`',
+                    'color': '#439FE0',
+                    'thumb_url': 'https://cdn4.iconfinder.com/data/icons/miscellaneous-icons-1/200/misc_game_combat-512.png'
+                }
+            ]
+        };
 
-  
-}
+        bot.reply(message, reply_with_attachments);
+
+    });
+};
